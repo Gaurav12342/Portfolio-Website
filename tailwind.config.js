3fc37a09b2c54edd5629d51c0f55271ee71e771e
Gaurav12342 <gauravpendharkar2@gmail.com>
2023-05-30 22:23:12 +0530

    Added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,css,scss}'],
+  // darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        typing: {
+          '0%, 100%': {width: '0%'},
+          '30%, 70%': {width: '100%'},
+        },
+        blink: {
+          '0%': {
+            opacity: 0,
+          },
+        },
+        'rotate-loader': {
+          '0%': {
+            transform: 'rotate(0deg)',
+            strokeDashoffset: '360%',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+            strokeDashoffset: '-360%',
+          },
+        },
+      },
+      screens: {
+        touch: {raw: 'only screen and (pointer: coarse)'},
       },
     },
   },
-  plugins: [],
-}
+  // eslint-disable-next-line no-undef
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
