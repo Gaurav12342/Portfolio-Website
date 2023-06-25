24467bb0b5a3d8fdb09d023a606311a3b27d8cee
Gaurav12342 <gauravpendharkar2@gmail.com>
2023-06-25 16:08:30 +0530

    Disabled some eslint rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { memo } from "react";
 import "tailwindcss/tailwind.css";
 
-const App = memo(({ Component, pageProps }: AppProps): JSX.Element => {
+const App = memo(({ Component, pageProps }: any): JSX.Element => {
   return <Component {...pageProps} />;
 });
 
